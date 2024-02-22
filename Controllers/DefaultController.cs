0d8cd84ebbc7e52e0c32c7fff64b9289e0781a0c
Hayrunnisa Bulut <hayrunnisa.bulut99@gmail.com>
2024-02-23 01:01:27 +0300

    Partial oluşturma

---
+++ Controllers/DefaultController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TasteFoodIt.Entities;
+using TasteFoodIt.Context;
+
+namespace TasteFoodIt.Controllers
+{
+    public class DefaultController : Controller
+    {
+        TasteContext context = new TasteContext();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult PartialHead()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialScript()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialNavbarInfo()
+        {
+            ViewBag.phone = context.Addresses.Select(x => x.Phone).FirstOrDefault();
+            ViewBag.email = context.Addresses.Select(y => y.Email).FirstOrDefault();
+            ViewBag.description = context.Addresses.Select(z => z.Description).FirstOrDefault();
+            return PartialView();
+        }
+        public PartialViewResult PartialNavbar()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialSlider()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialAbout()
+        {
+            ViewBag.title = context.Abouts.Select(x => x.Title).FirstOrDefault();
+            ViewBag.description = context.Abouts.Select(x => x.Description).FirstOrDefault();
+            ViewBag.image = context.Abouts.Select(x => x.ImageUrl).FirstOrDefault();
+            return PartialView();
+        }
+        public PartialViewResult PartialMenu() 
+        {
+            var values = context.Products.ToList();
+            return PartialView(values);
+        }
+        public PartialViewResult PartialTestimonial()
+        {
+            var values = context.Testimonials.ToList();
+            return PartialView(values);
+        }
+        public PartialViewResult PartialChef()
+        {
+            return PartialView();
+        }
+
+    }
+}
