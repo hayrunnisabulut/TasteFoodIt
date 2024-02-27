aec058ccc12f3ad1ad1affd13ca5126928dffc4d
Hayrunnisa Bulut <hayrunnisa.bulut99@gmail.com>
2024-02-27 20:52:59 +0300

    category admin sayfası oluşturma

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TasteFoodIt.Context;
 
 namespace TasteFoodIt.Controllers
 {
     public class CategoryController : Controller
     {
         // GET: Category
+        TasteContext context = new TasteContext();
         public ActionResult CategoryList()
         {
-            return View();
+            var values = context.Categories.ToList();
+            return View(values);
         }
     }
 }
