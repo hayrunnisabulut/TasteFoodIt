96add76a2184f8849420acfee98d779145b5d76e
Hayrunnisa Bulut <hayrunnisa.bulut99@gmail.com>
2024-02-27 20:36:57 +0300

    Partial oluşturulması

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TasteFoodIt.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        public ActionResult CategoryList()
+        {
+            return View();
+        }
+    }
+}
