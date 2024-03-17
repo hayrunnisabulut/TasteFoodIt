7f233d67818536f441dfeeecf15f4b43806dbaa8
Hayrunnisa Bulut <hayrunnisa.bulut99@gmail.com>
2024-03-17 21:53:34 +0300

    Admin  rezervasyon ekranı eklendi

---
+++ Controllers/OpenDayHoursController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TasteFoodIt.Context;
+using TasteFoodIt.Entities;
+
+namespace TasteFoodIt.Controllers
+{
+    public class OpenDayHoursController : Controller
+    {
+        TasteContext context = new TasteContext();
+        public ActionResult OpenDayHoursList()
+        {
+            var values = context.OpenDayHours.ToList();
+            return View(values);
+        }
+        [HttpGet]
+        public ActionResult CreateOpenDayHours()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateOpenDayHours(OpenDayHour openDayHour)
+        {
+            context.OpenDayHours.Add(openDayHour);
+            context.SaveChanges();
+            return RedirectToAction("OpenDayHoursList");
+        }
+
+        public ActionResult DeleteOpenDayHour(int id)
+        {
+            var value = context.OpenDayHours.Find(id);
+            context.OpenDayHours.Remove(value);
+            context.SaveChanges();
+            return RedirectToAction("OpenDayHoursList");
+
+        }
+
+        [HttpGet]
+        public ActionResult UpdateOpenDayHour(int id)
+        {
+            var value = context.OpenDayHours.Find(id);
+            return View(value);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateOpenDayHour(OpenDayHour openDayHour)
+        {
+            var value = context.OpenDayHours.Find(openDayHour.OpenDayHourId);
+            value.DayName = openDayHour.DayName;
+            value.OpenHourRange = openDayHour.OpenHourRange;
+            context.SaveChanges();
+            return RedirectToAction("OpenDayHoursList");
+        }
+    }
+}
---
+++ Controllers/ReservationController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TasteFoodIt.Context;
+using TasteFoodIt.Entities;
+
+namespace TasteFoodIt.Controllers
+{
+    public class ReservationController : Controller
+    {
+        // GET: Reservation
+        TasteContext context = new TasteContext();
+        public ActionResult ReservationList()
+        {
+            var values = context.Reservations.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public ActionResult CreateReservation()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateReservation(Reservation reservation)
+        {
+            context.Reservations.Add(reservation);
+            context.SaveChanges();
+            return RedirectToAction("ReservationList");
+        }
+
+        public ActionResult DeleteReservation(int id)
+        {
+            var value = context.Reservations.Find(id);
+            value.ReservationStatus = "Reddedildi";
+            context.SaveChanges();
+            return RedirectToAction("ReservationList");
+
+        }
+        public ActionResult ApproveReservation(int id)
+        {
+            var value = context.Reservations.Find(id);
+            value.ReservationStatus = "Onaylandı";
+            context.SaveChanges();
+            return RedirectToAction("ReservationList");
+
+        }
+        public ActionResult WaitReservation(int id)
+        {
+            var value = context.Reservations.Find(id);
+            value.ReservationStatus = "Beklemede";
+            context.SaveChanges();
+            return RedirectToAction("ReservationList");
+
+        }
+
+        [HttpGet]
+        public ActionResult UpdateReservation(int id)
+        {
+            var value = context.Reservations.Find(id);
+            return View(value);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateReservation(Reservation reservation)
+        {
+            var value = context.Reservations.Find(reservation.ReservationId);
+            value.ReservationStatus = reservation.ReservationStatus;
+            value.Email = reservation.Email;
+            value.Phone= reservation.Phone;
+            value.ReservationDate = reservation.ReservationDate;
+            value.Name = reservation.Name;
+            value.Time = reservation.Time;
+            value.GuestCount = reservation.GuestCount;
+            context.SaveChanges();
+            return RedirectToAction("ReservationList");
+        }
+    }
+}
